baddb52f327375e33bc65519faaa87aa98eeaaa2
davidlatwe <davidlatwe@gmail.com>
2020-04-15 14:33:59 +0800

    Add package interface

---
+++ pyblish_plot/__init__.py
@@ -0,0 +1,17 @@
+
+from .plot import plot
+from .dictail import (
+    parse,
+    VisitDict,
+    Code,
+    DictOp
+)
+
+
+__all__ = [
+    "plot",
+    "parse",
+    "VisitDict",
+    "Code",
+    "DictOp",
+]
